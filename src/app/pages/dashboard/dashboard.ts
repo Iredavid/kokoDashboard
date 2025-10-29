54cb4df135ba49006720fd3f5340d120d89b71fe
ireoluwadavid44 <ireoluwadavid44@gmail.com>
2025-10-29 23:35:59 +0000

    firebase studio

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
