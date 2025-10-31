3c8861dea661700c33e145d5b7ed6b96abb20003
ireoluwadavid44 <ireoluwadavid44@gmail.com>
2025-10-31 17:36:21 +0000

    curve clean

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,132 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { ChartConfiguration, ChartData, ChartEvent, ChartOptions, ChartType, DoughnutControllerChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, BaseChartDirective],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: 'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)',
+        fill: 'origin',
+      },
+      {
+        data: [180, 480, 770, 90, 1000, 270, 400],
+        label: 'Series C',
+        yAxisID: 'y1',
+        backgroundColor: 'rgba(255,0,0,0.3)',
+        borderColor: 'red',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+    ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  };
+  lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      y: {
+        position: 'left',
+      },
+      y1: {
+        position: 'right',
+        grid: {
+          color: 'rgba(255,0,0,0.3)',
+        },
+        ticks: {
+          color: 'red',
+        },
+      },
+    },
 
+    plugins: {
+      legend: { display: true },
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public doughnutChartOptions: ChartOptions<'doughnut'> = {
+    responsive: false,
+    cutout: '80%',
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    // 'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 100] },
+    ],
+  };
+
+  // public chartClicked({
+  //   event,
+  //   active,
+  // }: {
+  //   event: ChartEvent;
+  //   active: object[];
+  // }): void {
+  //   console.log(event, active);
+  // }
+
+  // public chartHovered({
+  //   event,
+  //   active,
+  // }: {
+  //   event: ChartEvent;
+  //   active: object[];
+  // }): void {
+  //   console.log(event, active);
+  // }
 }
--- src/app/topbar/topbar.ts
+++ src/app/topbar/topbar.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+import {MatBadgeModule} from '@angular/material/badge';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-topbar',
-  imports: [FormsModule],
+  imports: [FormsModule, MatBadgeModule,],
   templateUrl: './topbar.html',
   styleUrl: './topbar.scss',
 })
